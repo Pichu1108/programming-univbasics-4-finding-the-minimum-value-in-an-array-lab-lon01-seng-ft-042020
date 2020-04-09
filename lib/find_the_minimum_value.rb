1ebe6fe1e2b6ea1091b7915be63076e65b7c82e8
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-09 18:35:58 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,27 @@
+#def find_min_value(array)
+#  array.min
+#end
+
+#def find_max_value(array)
+  #count = 0
+  #max_value = -1
+#  while count < array.length do
+#    if max_value < array[count]
+#      max_value = array[count]
+  #  end
+#    count += 1
+#  end
+#  max_value
+#end
+
 def find_min_value(array)
-  array.min
+count = 0
+minimun_value = 1
+while count > array.length do
+  if minimun_value == array[count]
+    minimun_value = array[count]
+  end
+  count += 1
+end
+minimun_value
 end
